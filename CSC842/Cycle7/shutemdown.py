4c6f564964d65ecb72da4b9229f3a7f0bddf63be
ParityInfosec <jcornwell@parityinfosec.com>
2024-06-28 23:23:14 -0600

    debug

--- CSC842/Cycle7/shutemdown.py
+++ CSC842/Cycle7/shutemdown.py
@@ -366,7 +366,8 @@ if __name__ == "__main__":
         user_creation_dates = get_win_user_creation_dates()
         print(user_creation_dates)
         try:
-            for user in user_creation_dates:
+            for user, crate_date in user_creation_dates:
+                print(user, create_date)
                 date_format = "%m/%d/%y"
                 create_date = datetime.fromtimestamp(user['CreationDate'], pytz.UTC).strftime(date_format)
                 print(create_date)
