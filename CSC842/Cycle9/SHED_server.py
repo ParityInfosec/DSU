dbaf8a436b62f63f1d2cc3fdc9e819afc939b87f
Justin Cornwell <63657326+ParityInfosec@users.noreply.github.com>
2024-07-14 13:40:24 -1000

    Update SHED_server.py

--- CSC842/Cycle9/SHED_server.py
+++ CSC842/Cycle9/SHED_server.py
@@ -257,6 +257,7 @@ def ssh_launch(ip, port, OS, local_store_folder, path_elements=[], log_elements=
             # Execute the command
             ssh.exec_command(f'chmod +x {str(remote_path)}')
             output = execute_sudo_command(ssh, cmd)
+            print("ran successfully")
     finally:
         # Clean up & Close the connection
         if ssh:
