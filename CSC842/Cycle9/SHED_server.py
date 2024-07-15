6c1c8e9e2d01843b4e3ea1cd28842eff0e21827f
Justin Cornwell <63657326+ParityInfosec@users.noreply.github.com>
2024-07-14 14:07:46 -1000

    Update SHED_server.py

--- CSC842/Cycle9/SHED_server.py
+++ CSC842/Cycle9/SHED_server.py
@@ -273,8 +273,10 @@ def ssh_launch(ip, port, OS, local_store_folder, path_elements=[], log_elements=
             except FileNotFoundError as e:
                 print(f"Output folder could not be created: ... {e}")
             download_file_via_scp(ssh, log_path, dl_log)
-            ssh.exec_command(f'rm -rf {str(folder_path)}')
-            ssh.exec_command(f'rm -f {remote_path}')
+            stdout = ssh.exec_command(f'rm -rf {str(folder_path)}')
+            stdout.channel.recv_exit_status()  # Wait for command to complete
+            stdout = ssh.exec_command(f'rm -f {remote_path}')
+            stdout.channel.recv_exit_status()  # Wait for command to complete
             ssh.close()
 
 def ssh_all(test_systems):
